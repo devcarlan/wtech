73ea99019b5893cb92693cd2c6633075ae5073b1
Carlan Henry <devcarlan@gmail.com>
2025-03-01 23:07:38 -0500

    some clean up

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
 import Choose from './components/choose'
-import FAQ from './components/faq'
-import Footer from './components/footer'
 import Hero from './components/hero'
 import Services from './components/services'
 import Ship from './components/ship'
